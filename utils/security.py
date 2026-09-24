04955cec75570e9869467e2ec17153dc62e68a62
Fas Al Yashin <fasyashin@gmail.com>
2026-09-24 21:15:06 +0600

    Update security and schemas logic

--- utils/security.py
+++ utils/security.py
@@ -1,8 +1,9 @@
 from datetime import datetime, timedelta, timezone
 from jose import JWTError, jwt
 from passlib.context import CryptContext
+import os
 
-SECRET_KEY = "bloodbridge-secret-key-change-this-later"
+SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 60
 PASSWORD_RESET_EXPIRE_MINUTES = 15
